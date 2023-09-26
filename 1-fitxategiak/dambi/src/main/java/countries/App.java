951c297801da948b3b1bffa377045dc97879a141
Unai Zelaia-Zugadi <unaizelaiaz@gmail.com>
2023-09-26 13:31:33 +0200

    countries proiektuarekin jarraitu

--- 1-fitxategiak/dambi/src/main/java/countries/App.java
+++ 1-fitxategiak/dambi/src/main/java/countries/App.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.glassfish.jaxb.runtime.v2.schemagen.xmlschema.List;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
@@ -14,6 +17,13 @@ import jakarta.xml.bind.Marshaller;
 
 public class App {
     public static void main(String[] args) {
+
+
+        Countries countriesList = new Countries();
+        ArrayList<Country> list = new ArrayList<Country>();
+        
+
+
 
         Country spain = new Country();
         spain.setName("Spain");
@@ -25,32 +35,33 @@ public class App {
 
         Country portugal = new Country();
         portugal.setName("Portugal");
-        spain.setCapital("Madrid");
-        spain.setContinent("Europe");
-        spain.setImportance(1);
-        spain.setFoundation(LocalDate.of(1469, 10, 19));
-        spain.setPopulation(45000000);
+        portugal.setCapital("Lisboa");
+        portugal.setContinent("Europe");
+        portugal.setImportance(1);
+        portugal.setFoundation(LocalDate.of(1139, 10, 19));
+        portugal.setPopulation(10000000);
 
-        Countries list = new Countries();
+        list.add(portugal);
+        list.add(spain);
 
+        countriesList.setCountries(list);
 
         try {
             /* init jaxb marshaler */
-            JAXBContext jaxbContext = JAXBContext.newInstance(Country.class);
+            JAXBContext jaxbContext = JAXBContext.newInstance(Countries.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             
             new File("country.xml");
             OutputStream os = new FileOutputStream("country.xml");
-
 
             /* set this flag to true to format the output */
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
 
             /* marshaling of java objects in xml (output to file and standard output) */
-            jaxbMarshaller.marshal(spain, os);
+            jaxbMarshaller.marshal(countriesList, os);
         } catch (JAXBException e) {
-            System.out.println("Error: " + e.getMessage() + "Linked exceptions: " + e.getLinkedException());
+            System.out.println("Error: " + e.getMessage() + "\nLinked exceptions: " + e.getLinkedException());
         } catch (FileNotFoundException e){
             System.out.println("Error: " + e.getMessage());
         }
--- 1-fitxategiak/dambi/src/main/java/countries/Countries.java
+++ 1-fitxategiak/dambi/src/main/java/countries/Countries.java
@@ -4,19 +4,25 @@ import java.util.List;
 
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
-    @XmlRootElement( name = "Countries" )
-public class Countries
-{
-List countries;
+import jakarta.xml.bind.annotation.XmlSeeAlso;
 
-/**
-* element that is going to be marshaled in the xml
-*/
+@XmlRootElement(name = "Countries")
+@XmlSeeAlso({Country.class})
+public class Countries {
 
-@XmlElement( name = "Country" )
-public void setCountries( List countries )
-{
-    this.countries = countries;
-}
+    
+    List<Country> countries;
 
+    /**
+     * element that is going to be marshaled in the xml
+     */
+
+    public List getCountries(){
+        return countries;
+    } 
+
+    @XmlElement(name = "Country")
+    public void setCountries(List countries) {
+        this.countries = countries;
+    }
 }
